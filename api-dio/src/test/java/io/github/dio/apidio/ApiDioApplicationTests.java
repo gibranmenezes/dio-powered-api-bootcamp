7474ab744bdcabd92e4bb1b2c080c634b2a17779
gibranmenezes <gibranquimica@gmail.com>
2024-03-25 07:47:13 -0300

    adding security to api

---
+++ api-dio/src/test/java/io/github/dio/apidio/ApiDioApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dio.apidio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
